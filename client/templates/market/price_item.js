65e430c5b6a9631c5f4ab721a46b80eb25550e47
Maciej Nowakowski <m.nowakowskipl@gmail.com>
2015-06-20 14:52:56 +0100

    Correct myPrice? helper

--- client/templates/market/price_item.js
+++ client/templates/market/price_item.js
@@ -1,5 +1,18 @@
 Template.priceItem.helpers({
 	myPrice: function(){
-		return !!(Meteor.userId() === this.userId);
+		var currentUserId = Meteor.userId();
+		var pricesIds = this.prices;
+		var isMyPrice = false;
+
+		// Check all Prices connected to given Bid or Offer.
+		// Find the price and check if it's userId is same as current user Id
+		_.each(pricesIds, function(priceId){
+			var price = Prices.findOne(priceId);
+			if (price.userId === currentUserId) { 
+				isMyPrice = true; 
+			}
+		});
+
+		return isMyPrice;
 	}
 });
