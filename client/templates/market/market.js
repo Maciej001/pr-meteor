12fbc497d5c2673e51edde29865a52b59eaa0f1f
Maciej Nowakowski <m.nowakowskipl@gmail.com>
2015-06-30 21:47:31 +0100

    Last div fills rest of the page

--- client/templates/market/market.js
+++ client/templates/market/market.js
@@ -2,6 +2,27 @@
 // only button is displayed and not full form
 Template.market.onCreated(function() {
 	Session.set('newOrder', false);
+
+});
+
+Template.market.onRendered(function(){
+	// Set height of last div so it fills rest of the screen
+	var bodyHeight = 		$('body').height();
+	var headerHeight = 	50;
+	var sessionHeight = this.$("#sessionStatistics").height();
+	var pricesHeight = 	this.$("#prices-panel").height();
+
+	var remainingHeight = (bodyHeight - headerHeight - sessionHeight - pricesHeight).toString() + "px";
+
+
+	console.log('body h ', bodyHeight);
+	console.log('headerHeight ', headerHeight);
+	console.log('session he ', sessionHeight );
+	console.log('prices h ', pricesHeight);
+
+	console.log('remaining height ', remainingHeight);
+
+	$("#my-activity").css("min-height", remainingHeight );
 });
 
 Template.market.helpers({
@@ -20,3 +41,6 @@ Template.market.events({
 		Session.set('newOrder', true);
 	}
 });
+
+
+
