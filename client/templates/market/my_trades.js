f80815ba6fde6fcc440bbb3f434c1ec54bc22d11
Maciej Nowakowski <m.nowakowskipl@gmail.com>
2015-07-13 19:56:36 +0100

    Show trades only if exist

--- client/templates/market/my_trades.js
+++ client/templates/market/my_trades.js
@@ -2,4 +2,7 @@ Template.myTrades.helpers({
 	myTrades: function(){
 		return Trades.find({userId: Meteor.userId()}, { sort: {created_at: -1}, limit: 6 });
 	},
+	anyTrades: function(){
+		return !!Trades.findOne({ userId: Meteor.userId() });
+	}
 });
