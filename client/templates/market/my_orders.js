939f7aed2ce5b85b43f54a4c7b5a461609da8fd6
Maciej Nowakowski <m.nowakowskipl@gmail.com>
2015-07-13 19:50:06 +0100

    Display orders only if exist

--- client/templates/market/my_orders.js
+++ client/templates/market/my_orders.js
@@ -2,4 +2,10 @@ Template.myOrders.helpers({
 	myOrders: function(){
 		return Prices.find({userId: Meteor.userId()}, { sort: {created_at: -1}, limit: 6 });
 	},
+	anyActiveOrders: function(){
+		return !!Prices.findOne({ userId: Meteor.userId(), state: 'active' });
+	}, 
+	anyOrders: function(){
+		return !!Prices.findOne({ userId: Meteor.userId() });
+	}
 });
